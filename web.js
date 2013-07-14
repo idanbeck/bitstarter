1c95939305cd8575f2f4daa59083f9ee2f888d5b
Ubuntu <ubuntu@ip-172-31-36-84.us-west-2.compute.internal>
2013-07-14 06:26:09 +0000

    fixed encoding

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   var fsIndex = fs.readFileSync("index.html", function(err, data) {
     if(err) throw err;
-    console.log(data.toString());
+    console.log(data.toString('utf-8'));
   });
 });
 
